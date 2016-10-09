42d2aba40b055516a0659951b8c20255857fc607
Hoàng Quý <hoangquy1096@gmail.com>
2016-10-09 16:49:32 +0700

    Create functions.cs

---
+++ CSharp/functions.cs
@@ -0,0 +1,18 @@
+ Random User Agent from file C#
+ 
+ private static string RandomUa()
+        {
+            string[] lines;
+            var list = new List<string>();
+            var fileStream = new FileStream(Directory.GetCurrentDirectory() + "\\listua.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+            lines = list.ToArray();
+            return lines.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+        }
