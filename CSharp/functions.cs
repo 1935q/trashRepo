82ac8df0a88e9f3069d999363820ce4872629220
Hoàng Quý <hoangquy1096@gmail.com>
2016-10-09 16:50:02 +0700

    Update functions.cs

--- CSharp/functions.cs
+++ CSharp/functions.cs
@@ -1,18 +1,17 @@
- Random User Agent from file C#
- 
+ //Random User Agent from file C#
  private static string RandomUa()
-        {
-            string[] lines;
-            var list = new List<string>();
-            var fileStream = new FileStream(Directory.GetCurrentDirectory() + "\\listua.txt", FileMode.Open, FileAccess.Read);
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
-            {
-                string line;
-                while ((line = streamReader.ReadLine()) != null)
-                {
-                    list.Add(line);
-                }
-            }
-            lines = list.ToArray();
-            return lines.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
-        }
+ {
+     string[] lines;
+     var list = new List<string>();
+     var fileStream = new FileStream(Directory.GetCurrentDirectory() + "\\listua.txt", FileMode.Open, FileAccess.Read);
+     using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+     {
+         string line;
+         while ((line = streamReader.ReadLine()) != null)
+         {
+             list.Add(line);
+         }
+     }
+     lines = list.ToArray();
+     return lines.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+ }
